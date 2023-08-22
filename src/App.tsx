6de97326df8d5d5e09cf9f04d66a67dfce623624
LHBR <lucas.rocha5@estudante.ifms.edu.br>
2023-08-22 08:30:27 -0400

    correção

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Header from './componentes/header/Header'
-import Footer from './componentes/footer/Footer'
+// import Footer from './componentes/footer/Footer'
 import Footer1 from './componentes/Footer1/Footer1'
 function App() {
   return (
